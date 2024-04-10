fcc8fff0c51932cb16f9a406c8afb0849ebf0679
G0kuuu <gokulsabu02@gmail.com>
2024-04-10 10:23:20 +0530

    getting Movie details and Movie interface

--- actions/movieData.ts
+++ actions/movieData.ts
@@ -2,4 +2,7 @@ import { getApiResponse } from "@lib/requests"
 
 export const fetchTrending = async ()=>{
     const data = await getApiResponse("trending/movie/week")
+    const trending = data.results
+
+    return trending
 }
