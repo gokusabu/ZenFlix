9a719b843a1e4dc2c388fe969e11523651fa9878
G0kuuu <gokulsabu02@gmail.com>
2024-04-11 21:40:53 +0530

    search bar s

--- app/(root)/search/[query]/page.tsx
+++ app/(root)/search/[query]/page.tsx
@@ -1,13 +1,17 @@
-import Navbar from '@components/Navbar'
-
-
-const SearchPage = ({ params } : {params : {query: string} }) => {
-    const search = params.query
+import Navbar from "@components/Navbar";
+const SearchPage = ({ params }: { params: { query: string } }) => {
+  const search = params.query;
   return (
     <>
-    <Navbar/>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam rem
+        molestiae quos adipisci dolorem totam voluptates, sequi esse in incidunt
+        blanditiis iusto velit aliquam quas placeat voluptas doloribus ad
+        magnam.
+      </div>
+      <Navbar />
     </>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
