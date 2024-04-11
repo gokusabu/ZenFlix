5be5c0ac9216cda339e8c5c10f2bddcb754da753
G0kuuu <gokulsabu02@gmail.com>
2024-04-11 21:46:12 +0530

    removed div

--- app/(root)/search/[query]/page.tsx
+++ app/(root)/search/[query]/page.tsx
@@ -3,12 +3,6 @@ const SearchPage = ({ params }: { params: { query: string } }) => {
   const search = params.query;
   return (
     <>
-      <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam rem
-        molestiae quos adipisci dolorem totam voluptates, sequi esse in incidunt
-        blanditiis iusto velit aliquam quas placeat voluptas doloribus ad
-        magnam.
-      </div>
       <Navbar />
     </>
   );
